c4045e096a17a4a12a4fe80b053d30b740d4686f
Anabella La Padula <anabella.lapadula@gmail.com>
2022-11-05 15:01:02 -0300

    localStorage/ modificacion general de codigo Js.

--- archivo.js
+++ archivo.js
@@ -1,4 +1,4 @@
-const carrito= [];
+let carrito= [];
 class Producto {
     constructor (codigo, articulo, talle, color, precio, img ){
         this.codigo = codigo; 
@@ -9,7 +9,6 @@ class Producto {
         this.img=img;
     }
    
-// metodo
 }
 
 const producto01 = new Producto (0001, "remera", "m", "rojo", 2100, "./imagenes/remeranegra.jpg");
@@ -18,39 +17,20 @@ const producto03 = new Producto (0003, "remera", "m", "negro", 2506, "./imagenes
 const producto04 = new Producto (0004, "remera","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
 const producto05 = new Producto (0005, "remera","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
 const producto06 = new Producto (0006, "camiseta","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
-
-const productos = [ producto01, producto02, producto03,producto04, producto05, producto06];
-/*
-let busquedaPorTalleM = productos.filter((e)=> e.talle=="m")
-console.table(busquedaPorTalleM); 
-
-let busquedaPorTalleL = productos.filter((e)=> e.talle=="l")
-console.log(busquedaPorTalleL);
-
-let busquedaPorTalleS = productos.filter((e)=> e.talle=="s")
-console.log(busquedaPorTalleS);
-
-//filtrar por precios
-let filtrarPorPrecio = productos.filter((e)=> e.precio >= 2000)
-console.log(filtrarPorPrecio);
-let filtrarPorPrecio01 = productos.filter((e)=> e.precio <= 2000)
-console.log(filtrarPorPrecio01)
-
-*/
+const producto07 = new Producto (0006, "camiseta","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
+const stockProductos = [ producto01, producto02, producto03,producto04, producto05, producto06, producto07];
 
 
+document.addEventListener("DOMContentLoaded", () => {
+carrito = JSON.parse(localStorage.getItem("miCompra"))  || [] 
+    mostrarCompra();
+})
 
-carrito.splice (1 , 1 )
-
-
-
-//plantillas
-let divProductos = document.getElementById("productos")
-divProductos.className ="d-flex"
-
-productos.forEach((elemento)=>{
-    let card = document.createElement("div")
-card.innerHTML = `<div class="card m-2 ">
+//Renderizado
+const contenedor = document.getElementById("productos")
+contenedor.className="d-flex "
+stockProductos.forEach((elemento) => {
+contenedor.innerHTML += `<div class="card m-2 style="width: 18rem; ">
                         <img src="${elemento.img}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${elemento.articulo}</h5>
@@ -61,14 +41,55 @@ card.innerHTML = `<div class="card m-2 ">
                                 <option value="2">M</option>
                                 <option value="3">S</option>
                         </select> </br>
-                        <button  id="btn${elemento.codigo}" class="btn btn-primary">Comprar</button>
+                        <button  onclick="agregarProducto(${elemento.codigo})" class="btn btn-primary">Agregar al carrito</button>
                     </div>
                 </div>`
-divProductos.appendChild(card)
-
-
 })
 
+function agregarProducto(codigo){
+   const item = stockProductos.find((elemento)=>elemento.codigo === codigo)
+    carrito.push(item)
+    mostrarCompra()
+}
+
+const mostrarCompra = () =>{
+    const modalbody = document.querySelector(".modal .modal-body ")
+    modalbody.innerHTML= " "
+    carrito.forEach((prod) =>{
+        modalbody.innerHTML += ` 
+        <div class="modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${prod.img}"/>
+        </div>
+        <div>
+        <p>Producto: ${prod.articulo}</p>
+      <p>Precio: ${prod.precio}</p>
+      <button class="btn btn-danger"  onclick="eliminarCompra(${prod.codigo})">Eliminar</button>
+        </div>
+      </div>  `
+    })
+    storage();
+}
+
+
+function eliminarCompra (codigo){
+    const itemEliminado = codigo
+    carrito = carrito.filter((prod) => prod.codigo !== itemEliminado)
+    mostrarCompra()
+}
+
+
+
+function storage(){
+    localStorage.setItem("miCompra", JSON.stringify(carrito) )
+}
+
+
+
+
+
+
+/*
 productos.forEach((e)=> {
     document.getElementById(`btn${e.codigo}`).addEventListener("click", function(){
         agregarAlCarrito(e);
@@ -87,9 +108,18 @@ function agregarAlCarrito(productoAComprar){
         <td>$ ${productoAComprar.precio}</td>
     </tr>
 `;
+
+
+
+
 let totalCarrito = carrito.reduce((acumulador,prod)=>acumulador+prod.precio,0);
 document.getElementById("total").innerText = "Total a pagar $: "+totalCarrito;
-
 }
 
-cons
+let guardarProductos = JSON.stringify(productos);
+localStorage.setItem("misProductos",guardarProductos)
+
+let arrayParseado = JSON.parse(localStorage.getItem("misProductos"));
+console.log(arrayParseado)
+
+*/
