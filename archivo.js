f5840b0bc22fc9ba66ce6e4090849bd38c24dc1c
Anabella La Padula <anabella.lapadula@gmail.com>
2022-11-21 14:09:13 -0300

    aplicacion de json

--- archivo.js
+++ archivo.js
@@ -1,4 +1,7 @@
 let carrito= [];
+let eliminar = document.getElementById("vaciarCarrito")
+const sumadorCarrito = document.getElementById ("carritoContenedor")
+const totalCompra = document.getElementById ("precioTotal")
 class Producto {
     constructor (codigo, articulo, talle, color, precio, img ){
         this.codigo = codigo; 
@@ -10,40 +13,41 @@ class Producto {
     }
    
 }
+// Renderizado de Productos - Utilización de Fetch
+stockProductos = []
+const URLJSON= "productos.json"
+const contenedor = document.getElementById("productos")
+fetch(URLJSON)
+.then(response => response.json())
+.then( data => {
+    console.log(data)
+    for(let prod of data){
+        let prodNuevo = new Producto (prod.codigo, prod.articulo, prod.talle, prod.color, prod.precio, prod.img)
+        stockProductos.push(prodNuevo)
 
-const producto01 = new Producto (0001, "remera", "m", "rojo", 2100, "./imagenes/remeranegra.jpg");
-const producto02 = new Producto (0002, "remera","s", "negro", 1360, "./imagenes/remeranegra.jpg"  );
-const producto03 = new Producto (0003, "remera", "m", "negro", 2506, "./imagenes/remeranegra.jpg" );
-const producto04 = new Producto (0004, "remera","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
-const producto05 = new Producto (0005, "remera","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
-const producto06 = new Producto (0006, "camiseta","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
-const producto07 = new Producto (0006, "camiseta","l", "negro", 1598, "./imagenes/remeranegra.jpg" );
-const stockProductos = [ producto01, producto02, producto03,producto04, producto05, producto06, producto07];
+    }
+    stockProductos.forEach((elemento) => {
+        contenedor.innerHTML += `<div class="card ">
+                                <img src="${elemento.img}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${elemento.articulo}</h5>
+                                <p class="card-text">Color ${elemento.color}</p>
+                                <p class="card-text">Precio: $ ${elemento.precio}</p>
+            
+                                <button  onClick="agregarProducto(${elemento.codigo})" class="btn btn-primary">Agregar al carrito</button>
+                            </div>
+                        </div>`
+        })
+})
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
     carrito = JSON.parse(localStorage.getItem("miCompra")) ?? [] 
     mostrarCompra();
-})
-
-//Renderizado
-const contenedor = document.getElementById("productos")
-contenedor.className="d-flex "
-stockProductos.forEach((elemento) => {
-contenedor.innerHTML += `<div class="card m-2 style="width: 18rem; ">
-                        <img src="${elemento.img}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${elemento.articulo}</h5>
-                        <p class="card-text">${elemento.color}</p>
-                        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                                <option selected>Selecciona el talle </option>
-                                <option value="1">L</option>
-                                <option value="2">M</option>
-                                <option value="3">S</option>
-                        </select> </br>
-                        <button  onclick="agregarProducto(${elemento.codigo})" class="btn btn-primary">Agregar al carrito</button>
-                    </div>
-                </div>`
 })
 
 function agregarProducto(codigo){
@@ -76,6 +80,16 @@ const mostrarCompra = () =>{
         </div>
       </div>  `
     })
+
+    if(carrito.length=== 0){
+        modalbody.innerHTML=`
+        <p class = "text-center" > No hay productos agregados </p>
+        `
+    }
+
+    totalCompra.innerHTML = carrito.reduce((acumulador,prod)=>acumulador+prod.precio,0)
+
+    sumadorCarrito.textContent = carrito.length
     storage();
 }
 
@@ -92,42 +106,36 @@ function storage(){
     localStorage.setItem("miCompra", JSON.stringify(carrito) )
 }
 
-
-
-
-
+// vaciar carrito
+ eliminar.onclick = ()=>{
+    carrito = []; 
+    mostrarCompra()
+ }
 
 /*
-productos.forEach((e)=> {
-    document.getElementById(`btn${e.codigo}`).addEventListener("click", function(){
-        agregarAlCarrito(e);
-    })
-});
+ //Renderizado
 
+function renderizarPrductos(){
+    const URLJSON= "/productos.json"
+    fetch(URLJSON)
+       .then(response => response.json())
+       .then(data => {
+        const misProductos = data.productoJson
+        misProductos.forEach(producto => {
+            document.getElementById("divProdNuevo").innerHTML +=`<div class="card ">
+            <img src="${producto.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${producto.articulo}</h5>
+            <p class="card-text">${producto.color}</p>
 
-function agregarAlCarrito(productoAComprar){
-    carrito.push(productoAComprar);
-    document.getElementById("tablabody").innerHTML += `
-
-    <tr>
-        <td>${productoAComprar.codigo}</td>
-        <td>${productoAComprar.articulo}</td>
-        <td>${productoAComprar.color}</td>
-        <td>$ ${productoAComprar.precio}</td>
-    </tr>
-`;
-
-
-
-
-let totalCarrito = carrito.reduce((acumulador,prod)=>acumulador+prod.precio,0);
-document.getElementById("total").innerText = "Total a pagar $: "+totalCarrito;
+            <button  onClick="agregarProducto(${producto.codigo})" class="btn btn-primary">Agregar al carrito</button>
+        </div>
+    </div>`
+        })
+     
+       })
 }
 
-let guardarProductos = JSON.stringify(productos);
-localStorage.setItem("misProductos",guardarProductos)
-
-let arrayParseado = JSON.parse(localStorage.getItem("misProductos"));
-console.log(arrayParseado)
+renderizarPrductos()
 
 */
